e96313ba65bcfd256d93662a5029e22e83dc6e96
Filipe <filipeandrade000@gmail.com>
2022-12-16 23:30:29 -0300

    Completo

---
+++ src/main/java/br/com/mildevs/dao/VeiculoDAO.java
@@ -0,0 +1,65 @@
+package br.com.mildevs.dao;
+
+import java.util.List;
+
+import br.com.mildevs.entity.Veiculo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
+public class VeiculoDAO {
+
+    private EntityManager manager;
+
+    public VeiculoDAO() {
+        this.manager = Persistence.createEntityManagerFactory("sistema-transito").createEntityManager();
+    }
+
+    public boolean persistirVeiculo(Veiculo novoVeiculo) {
+        try {
+            this.manager.getTransaction().begin();
+            this.manager.persist(novoVeiculo);
+            this.manager.getTransaction().commit();
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean removerVeiculo(String placa) {
+        Veiculo veiculoEncontrado = this.manager.find(Veiculo.class, placa);
+
+        try {
+            this.manager.getTransaction().begin();
+            this.manager.remove(veiculoEncontrado);
+            this.manager.getTransaction().commit();
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean atualizarVeiculo(Veiculo veiculoModificado) {
+        try {
+            this.manager.getTransaction().begin();
+            this.manager.remove(veiculoModificado);
+            this.manager.getTransaction().commit();
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Veiculo consultarVeiculo(String placa) {
+        return this.manager.find(Veiculo.class, placa);
+    }
+
+    public List<Veiculo> listarVeiculos() {
+        Query consulta = this.manager.createQuery("SELECT v FROM Veiculo AS v");
+
+        return consulta.getResultList();
+    }
+}
